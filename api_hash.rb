1c646480965f8a369fb0661c2e3e138937523849
Minlieden <antonio.munoz.hernan@gmail.com>
2021-03-25 19:23:57 -0300

    se cambian elementos de estetica en HTML generado, y se quitan

--- api_hash.rb
+++ api_hash.rb
@@ -16,28 +16,17 @@ def obtener_datos(endpoint, api_key)
   http.verify_mode = OpenSSL::SSL::VERIFY_PEER
   response = http.request(request)
   result = JSON.parse(response.read_body)
-  hash = result
-  buid_web_page(hash)
+  buid_web_page(result)
 end
 
 def buid_web_page(hash)
   url_imagen = []
   hash.each do |photos, value|
-    print "photos #{photos}"
-    puts " "
-    
     value.each do |atributos|
-      print "atributos  #{atributos}"
-      puts " "
       atributos.each do |key, valores|
-        print "valores #{key} #{valores}"
-        puts " "
         valores = valores.to_s + " "
         if key == "img_src"
           url_imagen += valores.split(" ")
-          #print "url imagen #{url_imagen}"
-          #puts " "
-          
         end
       end
     end
@@ -48,20 +37,17 @@ def buid_web_page(hash)
   </head>
   <body>
     <ul>
-      <li>
-        "
+      "
+  
   url_imagen.each do |url|
-    pagina += "<img src= '#{url}'></li>\n        "
+    pagina += "<li><img src= '#{url}'></li>\n      "
   end
   pagina += "\n    </ul>
   </body>
   </html>"
   
-  
   File.write('index.html',pagina)
 end
-
-#print obtener_datos("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/", "yzcmtFAavpxRrh3CR0nQtGHs07tV9Nzx7l0LhuC4")
 
 obtener_datos("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/", "yzcmtFAavpxRrh3CR0nQtGHs07tV9Nzx7l0LhuC4")
 
